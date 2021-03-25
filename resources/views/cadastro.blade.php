99f381c97f9396fb93f10ec1b50fffe27ca9ecf6
igorleoo1 <l.igor@acad.ifma.edu.br>
2021-03-25 10:37:00 -0300

    Atualizacao2

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -5,8 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+    <div class="row mt-2">
+        <h3>Cabeçalho</h3>
+    </div>
+
     <h1>Cadastro</h1>
+
+    <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Nome Completo</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text"></div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Data de nascimento</label>
+            <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text"></div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Matrícula</label>
+            <input type="Number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text"></div>
+          </div>
+        <div class="mb-3 form-check">
+          
+          <label class="form-check-label" for="exampleCheck1"></label>
+        </div>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+      </form>
+</div>
+
+    <div class="row mt-2">
+        <h3>Rodapé</h3>
+    </div>
+</div>
 </body>
 </html>
